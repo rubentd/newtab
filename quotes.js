645866d2e80665259c5ce8305f39e46560ced80f
Ruben Torres <rubentdlh@gmail.com>
2024-02-04 19:40:43 -0300

    add new quotes

--- quotes.js
+++ quotes.js
@@ -148,4 +148,11 @@ If there are no gods, then you will be gone, but will have lived a noble life th
       `Treat what you don't have as non-existent. Look at what you have, the things you value most, and think of how much you'd crave them if you didn't have them. But be careful. Don't feel such satisfaction that you start to overvalue them—that it would upset you to lose them.`,
     ],
   },
+  {
+    title: 'George Bernard Shaw',
+    quotes: [
+      `This is the true joy in life, to be used for a purpose recognized by yourself as a mighty one, to be a force of nature instead of a feverish, selfish little clot of ailments and grievances complaining that the world will not devote itself to making you happy.
+I am of the opinion that my life belongs to the whole community and that as long as I live it is my privilege to do for it whatever I can. I want to be thoroughly used up when I die, for the harder I work the more I live, I rejoice in life for its own sake. Life is no brief candle to me, it is a sort of splendid torch which I\'ve got a hold of for a moment and I want to make it burn as brightly as possible before handing it on to future generations.`
+    ]
+  }
 ];
--- script.js
+++ script.js
@@ -56,44 +56,6 @@ const displayStories = async () => {
 
 displayStories();
 
-// load decrypt
-
-const loadDecryptStories = async () => {
-  const res = await window.fetch('https://api.decrypt.co/content/posts?_minimal=true&category=news&lang=en-US&offset=0&order=desc&orderby=date&per_page=12');
-  return await res.json();
-}
-
-const renderDecryptStories = (stories) => {
-  document.getElementById('decryptnews').innerHTML = '';
-  for (let story of stories) {
-    document.getElementById('decryptnews').innerHTML += `
-      <li>
-        <a href="${story.link}" target="_blank">
-          <img src="${story.custom_fields.featured_image_url}" alt="${story.custom_fields.featured_image_description}" />
-        </a>
-        <a class="overlay" href="${story.link}" target="_blank">${story.title.rendered}</a>
-      </li>
-    `;
-  }
-}
-
-const displayDecryptStories = async () => {
-  const date = new Date();
-  const todayKey = 'decrypt_stories' + date.toDateString() + date.getHours();
-  console.log(todayKey);
-  let stories = [];
-  const cacheStories = localStorage.getItem(todayKey);
-  if (cacheStories) {
-    stories = JSON.parse(cacheStories);
-  } else {
-    localStorage.clear();
-    const stories = await loadDecryptStories();
-    localStorage.setItem(todayKey, JSON.stringify(stories));
-  }
-  renderDecryptStories(stories);
-}
-
-displayDecryptStories();
 
 // load crypto prices
 
